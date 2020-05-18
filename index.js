5ebb417b4cd4d8b8940b6e755cf85c676dde497d
mgbanh97 <mgbanh97@gmail.com>
2020-05-18 16:44:50 +0700

    Added authentication with json web token

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
+
+// Initial Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.json({ msg: 'hello world' }));
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,75 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const auth = require('../middleware/auth');
+
+const User = require('../models/User');
 
 // @route   POST api/auth
-// @desc    Register a user
+// @desc    Authenticate user and get token
 // @access  Public
-router.post('/', (req, res) => {
-  res.send({ msg: 'User created successfully' });
-});
+router.post(
+  '/',
+  [
+    check('email', 'Please enter a vaild email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ msg: 'User is not exist' });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Invalid credential' });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 36000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Sever Error');
+    }
+  }
+);
 
 // @route   GET api/auth
-// @desc    Auth user and get token
+// @desc    Get logged in user
 // @access  Public
-router.get('/', (req, res) => {
-  res.send({ msg: 'User logged in successfully' });
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
 });
 
 module.exports = router;
